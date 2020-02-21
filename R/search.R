d0cf71544c19a8a72923936aefc4ac526f5a7ec4
Chris Lundberg <clundberg15@gmail.com>
2020-02-21 22:49:17 +0000

    added spar and account search functionality

--- R/search.R
+++ R/search.R
@@ -1,19 +1,19 @@
-#' Search FactSet File System
+#' Search your FactSet Filesystem
 #'
-#' A wrapper function that makes it slightly
-#' easier to authenticate with FactSet.
+#' This function makes it easy to search your FactSet filesystem. It returns a tibble with the accounts, directories, or documents in the specified directory.
 #'
-#' @param con A FactSet serial number.
-#' @param api_key A FactSet API key.
-#' @return A FactSet API connection object.
+#' @param con A FactSet connection object.
+#' @param dir A directory in the FactSet filesystem associated with your serial number.
+#' @param type The type of item you want to search for. Right now the options include accounts, directories, PA documents, or SPAR documents.
+#' @return A tibble containing the accounts, documents, or directories within the specified directory.
 #' @examples
-#' fs_con <- fs_auth(username, api_key)
-#' @seealso Visit \url{https://developer.factset.com/} to generate a key (FactSet license required).
+#' fs_pa_docs <- fs_search(con, dir, type = "pa")
+#' @seealso See \link{fs_auth} for authentication instructions.
 
 
 # Authenticate
 
-fs_search <- function(con, dir, type){
+fs_search <- function(con, dir, type = c("pa", "spar", "dir", "accounts")){
 
 
   if (type == "pa") {
@@ -21,18 +21,32 @@ fs_search <- function(con, dir, type){
 
     docs <- docs_con$GetPA3Documents(dir)$documents %>%
       unlist() %>% tibble::as_tibble() %>%
-      dplyr::transmute(pa_documents = value)
+      dplyr::transmute(pa_documents = stringr::str_remove_all(value, '\"'))
 
     return(docs)
 
   } else if (type == "spar") {
-    print("SPAR not implemented yet")
+    docs_con <- factset.analyticsapi.engines::DocumentsApi$new(con)
+
+    docs <- docs_con$GetSPAR3Documents(dir)$documents %>%
+      unlist() %>% tibble::as_tibble() %>%
+      dplyr::transmute(spar_documents = stringr::str_remove_all(value, '\"'))
 
   } else if (type == "dir") {
-    print("Directory search not implemented yet")
 
-  } else {
-    print("Accounts not implemented yet")
+    accounts_con <- factset.analyticsapi.engines::AccountsApi$new(con)
+
+    accounts <- accounts_con$GetAccounts(dir)$directories %>%
+      unlist() %>% tibble::as_tibble() %>%
+      dplyr::transmute(directories = stringr::str_remove_all(value, '\"'))
+
+  } else if (type == "accounts") {
+
+    accounts_con <- factset.analyticsapi.engines::AccountsApi$new(con)
+
+    accounts <- accounts_con$GetAccounts(dir)$accounts %>%
+      unlist() %>% tibble::as_tibble() %>%
+      dplyr::transmute(accounts = stringr::str_remove_all(value, '\"'))
 
   }
 }
